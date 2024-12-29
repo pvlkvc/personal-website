7613d201d559621729bad2f6ee1bc574ea19e079
pvlkvc <pvlkvc@users.noreply.github.com>
2024-12-29 23:28:34 +0000

    deploy: 3dc5f5d66b6c390edff4c71520b583f17d899ccf

---
+++ assets/About-B0hVemsH.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DSkqGNzY.js";function t(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"horizontal-container",children:e.jsx("p",{children:"Oops! Nothing is here yet. The website is still being built!"})})})}export{t as default};
--- assets/Home-BO9dAQc6.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BAeFL7-f.js";function n(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"horizontal-container",children:e.jsx("p",{children:"Home"})})})}export{n as default};
