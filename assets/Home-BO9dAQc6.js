49cde28e69a8947866c790e00b1f0e4a1728e2c5
pvlkvc <pvlkvc@users.noreply.github.com>
2024-12-29 16:51:28 +0000

    deploy: 48a2b29fdf81175bde2f5963b7db6441f70e7221

---
+++ assets/Home-BO9dAQc6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BAeFL7-f.js";function n(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"horizontal-container",children:e.jsx("p",{children:"Home"})})})}export{n as default};
