2c64e9d23e0dd73ed500add363cf6c7acd875757
pvlkvc <pvlkvc@users.noreply.github.com>
2024-12-30 02:21:24 +0000

    deploy: 16938ad33d2703feb1d15a93e60ed5a9e8c1b095

--- assets/About-B0hVemsH.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DSkqGNzY.js";function t(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"horizontal-container",children:e.jsx("p",{children:"Oops! Nothing is here yet. The website is still being built!"})})})}export{t as default};
--- assets/Home-DfNDFFVN.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DSkqGNzY.js";function n(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"horizontal-container",children:e.jsx("p",{children:"Home"})})})}export{n as default};
