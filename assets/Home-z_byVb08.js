2c0cc5d7173d37a195fbbadbed4a8020f4cbc50b
pvlkvc <pvlkvc@users.noreply.github.com>
2025-01-04 19:59:57 +0000

    deploy: 3a5cda0a0ea44772ed2f6afcfaf0472e15711a8d

--- assets/Home-z_byVb08.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-uVR5s811.js";function r(){return n.jsx(n.Fragment,{children:n.jsx("p",{children:"Hi! My name is Anna :)"})})}export{r as default};
--- assets/Portfolio-nqX_aNGc.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-uVR5s811.js";function t(){return o.jsx(o.Fragment,{children:o.jsx("p",{children:"Portfolio page"})})}export{t as default};
