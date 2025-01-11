bdb2dfcaa1d7eacceb821dcc64804b4f47768fcd
Anna Pawlukiewicz <pvlkvc.anna@gmail.com>
2025-01-11 19:26:02 +0100

    uploaded dummy project data

---
+++ src/js/projects.js
@@ -0,0 +1,26 @@
+export const projectData = [
+  {
+    title: "Potential Project 1",
+    description:
+      "Description of Project 1 including information about what it is and what it was made with",
+    img: "src/assets/mitch_face.png",
+    tech: ["Python", "Numpy"],
+    link: "link to project",
+  },
+  {
+    title: "Potential Project 2",
+    description:
+      "Description of Project 2 including information about what it is and what it was made with",
+    img: "src/assets/mitch_face.png",
+    tech: ["Javascript", "React", "MongoDB"],
+    link: "link to project",
+  },
+  {
+    title: "Potential Project 3",
+    description:
+      "Description of Project 3 including information about what it is and what it was made with",
+    img: "src/assets/mitch_face.png",
+    tech: ["Javascript", "React"],
+    link: "link to project",
+  },
+];
