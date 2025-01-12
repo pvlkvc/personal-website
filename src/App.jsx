35341b632e689405ffefb52b77f1747ded684985
Anna Pawlukiewicz <pvlkvc.anna@gmail.com>
2025-01-12 17:10:21 +0100

    fixed broken import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./css/App.css";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
-import Portfolio from "./images/Portfolio.jsx";
+import Portfolio from "./components/Portfolio.jsx";
 
 export default function App() {
   return (
